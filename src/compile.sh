9d40560df8406b3c4010654ddb709b6f3ac66377
Reggie McLean <reggieman00@gmail.com>
2016-02-08 17:45:53 +0000

    Provided basic framework.

---
+++ src/compile.sh
@@ -0,0 +1,1 @@
+gcc `pkg-config --cflags gtk+-3.0` main.c -o checkers `pkg-config --libs gtk+-3.0`
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char** argv){
+    gtk_init(&argc, &argv);
+    
+    return 0;
+}
