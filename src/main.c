fd3f35d1d545842c4e95061981633abe99b659d4
Reggie McLean <reggieman00@gmail.com>
2016-02-10 17:14:48 +0000

    Another baby step.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <gtk/gtk.h>
 
 int main(int argc, char** argv){
+    GtkWindow *window;
     gtk_init(&argc, &argv);
-    
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(window, "Checkers");
+    gtk_window_set_default_size(window, 1024, 768);
+    gtk_main();
     return 0;
 }
