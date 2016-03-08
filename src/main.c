1007a9d2dcafcf69992164fbfeafc7e35988a5c8
Reggie McLean <reggieman00@gmail.com>
2016-03-08 14:47:03 +0000

    Added checker board. Untested.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <gtk/gtk.h>
+#include <gio/gio.h>
+#include <glib.h>
 #include "network.h"
 
 int main(int argc, char** argv){
@@ -6,6 +8,11 @@ int main(int argc, char** argv){
     GtkWidget *mainBox, *topBox, *bottomBox, *topRightBox, *topLeftBox, *checkersRows[8];
     GtkWidget *bottomLeftBox, *bottomRightBox;
     GtkWidget *checkersGrid[8][8];
+    const char *cssPath = "main.css";
+    GFile *css;
+    GtkCssProvider *cssProvider;
+    GError **error;
+    GtkStyleContext *opponentBoard, *playerBoard;
     int i, j;
     //initializing and packing
     gtk_init(&argc, &argv);
@@ -48,6 +55,28 @@ int main(int argc, char** argv){
     }
     //make it close when closed
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    //checkerboard
+    css = g_file_new_for_path(cssPath);
+    if(!gtk_css_provider_load_from_file(cssProvider, css, error)){
+        fprintf(stderr, "Could not open main.css");
+        return 1;
+    }
+    for(i = 0; i<8; i++){
+        for(j = 0; j<8; j++){
+            gtk_widget_set_size_request(checkersGrid[i][j], 50, 50);
+            if(i % 2)
+                if(j % 2)
+                    gtk_style_context_add_class(gtk_widget_get_style_context(checkersGrid[i][j]), "opponentBoard");
+                else
+                    gtk_style_context_add_class(gtk_widget_get_style_context(checkersGrid[i][j]), "playerBoard");
+            else
+                if(j % 2)
+                    gtk_style_context_add_class(gtk_widget_get_style_context(checkersGrid[i][j]), "playerBoard");
+                else
+                    gtk_style_context_add_class(gtk_widget_get_style_context(checkersGrid[i][j]), "opponentBoard");
+        }
+    }
+    //start
     gtk_widget_show(window);
     gtk_main();
     return 0;
